2a1d07548671471bfbe582428ff92120a1cfca6a
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-12-31 19:48:16 -0300

    configure `typeorm` and `mysql`docker container

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import { Router, Request, Response } from 'express';
-
+import "reflect-metadata"
+import AppDataSource from "../src/infra/data-source"
 const app = express();
 const PORT = 3000;
 const route = Router();
 
 app.use(express.json());
 
-route.get("/", (req: Request, res: Response) => {
+route.get("/status", (req: Request, res: Response) => {
+    const version = AppDataSource.driver.version
+
     res.json({
-        message: 'Hello world!'
+        message: version
     })
 })
 
