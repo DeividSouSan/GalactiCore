10e1db979596682ea9b9e534d145be640e9fa207
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-01-02 19:10:40 -0300

    configure jest and fix `dev` script

---
+++ src/routes/StellarSystem.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import database from '../infra/data-source'
+
+const router = express.Router()
--- tests/spaceships.test.ts
+++ tests/spaceships.test.ts
@@ -1,7 +1,44 @@
-import { describe, expect, test } from '@jest/globals';
 
 describe('spaceships entity routes', () => {
-    test('test_get_200', () => {
+    test('post_should_return_200', async () => {
+        const response = await fetch("http://localhost:3000/spaceships/", {
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify({
+                "model": "2025",
+                "manufacturer": "God",
+                "capacity": 12
+            })
+        })
 
+        expect(response.status).toEqual(200)
+    });
+
+    test('get_to_spaceships_should_return_200', async () => {
+        const response = await fetch("http://localhost:3000/spaceships/")
+
+        expect(response.status).toEqual(200)
+    });
+
+    test('get_to_spaceships_id_should_return_200', async () => {
+        const response = await fetch("http://localhost:3000/spaceships/1")
+
+        expect(response.status).toEqual(200)
+    });
+
+    test('put_to_spaceships_id_should_return_200', async () => {
+        const response = await fetch("http://localhost:3000/spaceships/")
+
+        expect(response.status).toEqual(200)
+    });
+
+    test('delete_to_spaceships_id_should_return_200', async () => {
+        const response = await fetch("http://localhost:3000/spaceships/2", {
+            method: 'DELETE',
+        })
+
+        expect(response.status).toEqual(200)
     });
 });
