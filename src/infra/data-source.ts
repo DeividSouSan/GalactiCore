8ca8ee74e98228fe59e636053da94727e9accd53
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-01-04 17:58:34 -0300

    environment variables with `.env.development` and dotenv

--- src/infra/data-source.ts
+++ src/infra/data-source.ts
@@ -4,14 +4,21 @@ import { StellarSystem } from "../entity/StellarSystem"
 import { Character } from "../entity/Characters"
 import { Planet } from "../entity/Planets"
 import { Spaceship } from "../entity/Spaceships"
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: ".env.development"
+})
+
+console.log("Teste ", process.env.MYSQL_DATABASE);
 
 const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "local_user",
-    password: "local_password",
-    database: "galaxy",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     entities: [StellarSystem, Character, Planet, Spaceship],
     migrations: ["src/migrations/*"],
 })
