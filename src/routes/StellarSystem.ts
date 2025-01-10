4bc18ecb8db3bee4dcc61a1ad9873a0b425b78c6
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-01-10 18:24:38 -0300

    finished `/stellar-systems` routes

--- src/routes/StellarSystem.ts
+++ src/routes/StellarSystem.ts
@@ -9,9 +9,6 @@ const router = express.Router()
 
 router.post("/", (req: Request, res: Response) => {
     const repository: Repository<StellarSystem> = database.getRepository(StellarSystem);
-
-    console.log(req.body);
-
     const newStellarSystem: StellarSystem[] = repository.create(req.body);
 
     repository.save(newStellarSystem);
@@ -23,10 +20,59 @@ router.post("/", (req: Request, res: Response) => {
 
 router.get("/", async (req: Request, res: Response) => {
     const repository: Repository<StellarSystem> = database.getRepository(StellarSystem);
+    const allStellarSystems: StellarSystem[] = await repository.find();
 
-    const stellarSystems: StellarSystem[] = await repository.find();
-
-    res.status(HTTPStatus.OK).send(stellarSystems);
+    res.status(HTTPStatus.OK).send(allStellarSystems);
 })
+
+router.get("/:id", async (req: Request, res: Response) => {
+    const repository: Repository<StellarSystem> = database.getRepository(StellarSystem);
+    const requestedId: number = parseInt(req.params.id);
+
+    try {
+        const stellarSystem: StellarSystem = await repository.findOneBy({
+            id: requestedId
+        });
+
+        if (stellarSystem === null) { throw Error(); };
+
+        res.status(HTTPStatus.OK).send(stellarSystem);
+    } catch (error) {
+        res.status(HTTPStatus.NOT_FOUND).send({});
+    }
+
+})
+
+router.put("/:id", async (req: Request, res: Response) => {
+    const repository: Repository<StellarSystem> = database.getRepository(StellarSystem);
+    const id: number = parseInt(req.params.id);
+    const body = req.body;
+
+    try {
+        await repository.update({ id: id }, body);
+
+        res.status(HTTPStatus.OK).send({
+            "updated": true
+        })
+    } catch {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).send({
+            "updated": false
+        })
+    }
+})
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    const repository: Repository<StellarSystem> = database.getRepository(StellarSystem);
+    const id: number = parseInt(req.params.id);
+
+    const stellarSystemObject = await repository.delete({ id: id });
+
+    console.log(stellarSystemObject)
+
+    res.status(HTTPStatus.OK).send({
+        "deleted": true
+    })
+})
+
 
 export default router;
