33a0357987986d653f9059f841dd60e8403d9e18
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-01-14 19:02:49 -0300

    starts refactoring `/spaceships` routes

--- src/routes/Spaceship.ts
+++ src/routes/Spaceship.ts
@@ -3,27 +3,56 @@ import { Request, Response } from 'express';
 import database from '../infra/data-source';
 import { Spaceship } from '../entity/Spaceships';
 import { Repository } from 'typeorm';
+import HTTPStatus from 'http-status-codes';
 
 const router = express.Router();
 
 router.post('/', (req: Request, res: Response) => {
     const repository: Repository<Spaceship> = database.getRepository(Spaceship);
-    const spaceship = repository.create(req.body)
 
-    repository.save(spaceship);
+    try {
+        const spaceship = repository.create(req.body)
+        repository.save(spaceship);
 
-    res.send({
-        result: "Created"
-    })
+        res.status(HTTPStatus.CREATED).json({
+            "status": "success",
+            "message": "Requisição processada com sucesso",
+            "data": {
+                spaceship
+            }
+        })
+    } catch (error) {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+            "status": "error",
+            "message": "Ocorreu um erro ao processar a requisição",
+            "data": {}
+        })
+    }
+
+
+
 })
 
 router.get('/', async (req: Request, res: Response) => {
     const repository = database.getRepository(Spaceship);
-    const spaceships = await repository.find();
+    const [spaceships, countSpaceships] = await repository.findAndCount();
 
-    res.send({
-        result: spaceships
-    })
+    try {
+        res.status(HTTPStatus.OK).json({
+            "status": "success",
+            "message": "Requisição processada com sucesso",
+            "data": {
+                countSpaceships,
+                spaceships
+            }
+        })
+    } catch (error) {
+        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+            "status": "error",
+            "message": "Ocorreu um erro ao processar a requisição",
+            "data": {}
+        })
+    }
 })
 
 router.get('/:id', async (req: Request, res: Response) => {
